10739eed413f23b43ef738c79f15e9358eb6d42f
uyvr97 <80188053+uyvr97@users.noreply.github.com>
2024-03-26 17:40:29 +0900

    Title: 문자에 따른 명령 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240326/문자에 따른 명령 2/text-based-commands2.cpp
+++ 240326/문자에 따른 명령 2/text-based-commands2.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int dx[]={1,-1,0,0}, dy[]={0,0,1,-1};
+int dx[]={0,1,0,-1}, dy[]={1,0,-1,0};
 
 int main() {
     // 여기에 코드를 작성해주세요.
     string cmd;
-    int dir = 2;
+    int dir = 0;
     int x = 0, y = 0;
     cin >> cmd;
-    for(int i = 0 ;i  < cmd.size();i++){
+    for(int i = 0; i < cmd.size(); i++){
         if(cmd[i] == 'F'){
             x += dx[dir];
             y += dy[dir];
@@ -21,6 +21,6 @@ int main() {
             dir = (dir + 1) % 4;
         }
     }
-    cout << x << " "<< y;
+    cout << x << " " << y;
     return 0;
 }
