a4fce6c37a3f695e39ab2d5dd9c963aadcbde59c
uyvr97 <80188053+uyvr97@users.noreply.github.com>
2024-04-02 21:53:59 +0900

    Title: 최소 경로로 탈출 하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240402/최소 경로로 탈출 하기/escape-with-min-distance.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int n,m;
+int arr[100][100], dist[100][100];
+bool visit[100][100];
+int dx[] = {0,0,1,-1}, dy[] = {1,-1,0,0};
+queue<pair<int,int>> q;
+
+bool InRange(int x, int y){
+    return (x >= 0 && x < n && y >= 0 && y < m);
+}
+
+void BFS(){
+    while(!q.empty()){
+        int x = q.front().first, y = q.front().second;
+        q.pop();
+        for(int i = 0; i < 4; i++){
+            int nx = x + dx[i], ny = y + dy[i];
+            if(!InRange(nx, ny))continue;
+            if(arr[nx][ny] == 1 && vit[nx][ny] == false){
+                visit[nx][ny] = true;
+                dist[nx][ny] = dist[x][y] + 1;
+                q.push({nx, ny});
+            }
+        }
+    }
+}
+
+
+int main() {
+    cin >> n >> m;
+    for(int i = 0 ; i < n; i++){
+        for(int j = 0; j < m ;j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    q.push({0,0});
+    visit[0][0] = 1;
+    BFS();
+
+    if(visit[n-1][m-1]) cout << dist[n-1][m-1];
+    else cout << -1;
+
+    return 0;
+}
