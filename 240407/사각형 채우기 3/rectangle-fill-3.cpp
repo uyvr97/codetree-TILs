61e852f0c7d6f1281406b5b90620fd1a42973aed
uyvr97 <80188053+uyvr97@users.noreply.github.com>
2024-04-07 12:50:32 +0900

    Title: 사각형 채우기 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240407/사각형 채우기 3/rectangle-fill-3.cpp
+++ 240407/사각형 채우기 3/rectangle-fill-3.cpp
@@ -7,10 +7,14 @@ int arr[1001];
 int main() {
     int n;
     cin >> n;
+    arr[0] = 1;
     arr[1] = 2;
     arr[2] = 7;
     for(int i = 3; i <= n; i++){
-        arr[i] = (arr[i-1]*2 + arr[i-2]*3 + 2) % MAX;
+        arr[i] = (arr[i-1]*2 + arr[i-2]*3) % MAX;
+        for(int j = i-3; j >= 0; j--){
+            arr[i] = (arr[i] + arr[j]*2) % MAX ;
+        }
     }
 
     cout << arr[n];
