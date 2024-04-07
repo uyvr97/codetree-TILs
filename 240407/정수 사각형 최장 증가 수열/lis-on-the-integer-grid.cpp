a4ae56bbde3b3f1a3aea59ad6672d44117c86ebd
uyvr97 <80188053+uyvr97@users.noreply.github.com>
2024-04-07 15:25:15 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 103ms, Memory: 21MB, Status: Passed - CodeTree

--- 240407/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
+++ 240407/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
@@ -1,53 +1,43 @@
 #include <iostream>
-#include <queue>
+#include <cstring>
 using namespace std;
 
 int n;
 int arr[501][501], dp[501][501];
 int dx[] = {0,0,1,-1}, dy[] = {1,-1,0,0};
-queue<pair<int,int>> q;
 
 bool InRange(int x, int y){
     return (x >= 1 && x <= n && y >= 1 && y <= n);
 }
 
-int count(){
-    int cnt = 0;
-    while(!q.empty()){
-        int r = q.front().first, c = q.front().second;
-        q.pop();
-        for(int i = 0; i < 4; i++){
-            int nx = r + dx[i], ny = c + dy[i];
-            if(InRange(nx, ny) && arr[nx][ny] > arr[r][c]){
-                dp[r][c]++;
-                if(dp[nx][ny] != 0){
-                    cnt += dp[nx][ny];
-                }
-                else {
-                    q.push({nx, ny});
-                    cnt++;
-                }
-            }
+void count(int x, int y){ //dfs
+    if(dp[x][y] > 1) return;
+    for(int i = 0; i < 4; i++){
+        int cnt = 1;
+        int nx = x + dx[i], ny = y + dy[i];
+        if(InRange(nx, ny) && arr[nx][ny] > arr[x][y]){ //dp[nx][ny]가 0이 아니면 종료, 0이면 재귀 
+            count(nx, ny);
+            cnt += dp[nx][ny];
         }
+        dp[x][y] = max(cnt, dp[x][y]);
     }
-    return cnt;
+    return;
 }
 
 int main() {
     cin >> n;
     for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= n; j++) 
+        for(int j = 1; j <= n; j++){
             cin >> arr[i][j];
+            dp[i][j] = 1;
+        }
     }
 
     int rel = 0;
    for(int i = 1; i <= n; i++){
-        for(int j= 1; j <=n; j++){
-            if(dp[i][j] == 0){
-                q.push({i,j});
-                dp[i][j] = count();
-                rel = max(rel, dp[i][j]);
-            }
+        for(int j = 1; j <=n; j++){
+            count(i, j);
+            rel = max(rel, dp[i][j]);
         }
    }
 
